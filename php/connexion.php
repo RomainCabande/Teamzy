ce8f17d6a741dd1100197661b4ad00e799f056f1
RomainCabande <rom.cabande@gmail.com>
2022-12-16 11:06:36 +0100

    Update connexion.php

--- php/connexion.php
+++ php/connexion.php
@@ -1,18 +1,17 @@
 <?php
-    $server = "localhost";
-    $login = "kvefnnxj_user";
-    $mdp = "User4phpmyadminpassword";
-    $db = "kvefnnxj_teamzy";
-    $userLogin = $_POST["username"];
-
-    ///Connexion au serveur MySQL
-    try {
-        $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-    }
-    catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
-
+     $server = "localhost";
+     $login = "kvefnnxj_user";
+     $mdp = "User4phpmyadminpassword";
+     $db = "kvefnnxj_teamzy";
+     $userLogin = $_POST["username"];
+ 
+     ///Connexion au serveur MySQL
+     try {
+         $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp, array(PDO::ATTR_PERSISTENT => true));
+     }
+     catch (Exception $e) {
+         die('Erreur : ' . $e->getMessage());
+     }
     $selectLoginPassword = 'SELECT login, hashed_mdp FROM entraineur';
     foreach  ($linkpdo->query($selectLoginPassword) as $row) {
         $loginFromBDD = $row['login'];
@@ -20,7 +19,7 @@
     }
 
     if($userLogin == $loginFromBDD AND password_verify($_POST["password"], $passwordHashedFromBDD)) {
-        header('Location: ../accueil.html');
+        header('Location: ../html/accueil.html');
     } else {
         header('Location: ../index.html');
     }
